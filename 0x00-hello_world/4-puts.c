6cf34b41176f1732c241c6a3dcb0aa7dc6e64946
Issam <issam.zouid14@gmail.com>
2023-02-16 16:51:04 -0800

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
+/**
+ * main - A programme that prints a lin woith puts function
+ * Return 0;
+ */
 int main(void)
 {
-puts("Programming is like building a multilingual puzzle");
-retur 0;
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }	
