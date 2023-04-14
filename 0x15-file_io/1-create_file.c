b6eb6d101d2d88c86ed4b5759705baa5374299bd
issam.zouaid <issam.zouid14@gmail.com>
2023-04-14 14:48:29 -0700

    task1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 #include <stdlib.h>
-#include <fcntl.h>
 #include <unistd.h>
+#include <fcntl.h>
 
 
 /**
@@ -13,36 +13,33 @@
  * @text_content: A pointer to a string to write to the file.
  *
  * Return: If the function fails - -1.
- *
  *         Otherwise - 1.
  */
 int create_file(const char *filename, char *text_content)
 {
-    int fd, w, len = 0;
+        int fd, w, len = 0;
 
-    if (filename == NULL)
-        return (-1);
+        if (filename == NULL)
+                return (-1);
 
-    if (text_content != NULL)
-    {
-        while (text_content[len])
-            len++;
-    }
+        fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+        if (fd == -1)
+                return (-1);
 
-    fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+        if (text_content != NULL)
+        {
+                while (text_content[len] != '\0')
+                        len++;
+        }
 
-    if (fd == -1)
-        return (-1);
+        w = write(fd, text_content, len);
+        if (w == -1)
+        {
+                close(fd);
+                return (-1);
+        }
 
-    w = write(fd, text_content, len);
-
-    if (w == -1)
-    {
         close(fd);
-        return (-1);
-    }
-
-    close(fd);
-    return (1);
+        return (1);
 }
 
