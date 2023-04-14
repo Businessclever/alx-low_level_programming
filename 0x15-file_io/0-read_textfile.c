ac9166524dffa586f44c707089e722a2eeb0ebf4
issam.zouaid <issam.zouid14@gmail.com>
2023-04-14 14:33:44 -0700

    task 0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,31 +1,50 @@
 #include "main.h"
+
 #include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+
 
 /**
- * read_textfile - Read text file and print to STDOUT.
- *
- * @filename: Text file being read.
- * @letters: Number of letters to be read.
- *
- * Return: Actual number of bytes read and printed on success.
- *         0 when function fails or filename is NULL.
+
+ * read_textfile- Read text file print to STDOUT.
+
+ * @filename: text file being read
+
+ * @letters: number of letters to be read
+
+ * Return: w- actual number of bytes read and printed
+
+ *        0 when function fails or filename is NULL.
+
  */
+
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-        char *buf;
-        ssize_t fd, w, t;
+        char buf[1024];
+        ssize_t fd, nread, nwritten = 0;
+
+        if (!filename)
+                return (0);
 
         fd = open(filename, O_RDONLY);
         if (fd == -1)
                 return (0);
 
-        buf = malloc(sizeof(char) * letters);
-        t = read(fd, buf, letters);
-        w = write(STDOUT_FILENO, buf, t);
+        while ((nread = read(fd, buf, sizeof(buf))) > 0) {
+                if (nwritten + nread > letters)
+                        nread = letters - nwritten;
+                if (write(STDOUT_FILENO, buf, nread) != nread) {
+                        close(fd);
+                        return (0);
+                }
+                nwritten += nread;
+                if (nwritten == letters)
+                        break;
+        }
 
-        free(buf);
         close(fd);
 
-        return (w);
+        return (nwritten);
 }
 
