ccb87d652dd9ef01709045dfe2dc7df6fbeacaf4
issam.zouaid <issam.zouid14@gmail.com>
2023-04-14 14:36:49 -0700

    task 0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,50 +1,53 @@
 #include "main.h"
-
+#include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <fcntl.h>
 
-
 /**
-
- * read_textfile- Read text file print to STDOUT.
-
- * @filename: text file being read
-
- * @letters: number of letters to be read
-
- * Return: w- actual number of bytes read and printed
-
- *        0 when function fails or filename is NULL.
-
+ * read_textfile - reads a text file and prints it to POSIX standard output
+ * @filename: name of the file to read
+ * @letters: number of letters to read and print
+ * Return: the actual number of letters read and printed, or 0 if failed
  */
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-        char buf[1024];
-        ssize_t fd, nread, nwritten = 0;
+	int fd, read_count, write_count;
+	char *buf;
 
-        if (!filename)
-                return (0);
+	if (filename == NULL)
+		return (0);
 
-        fd = open(filename, O_RDONLY);
-        if (fd == -1)
-                return (0);
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
 
-        while ((nread = read(fd, buf, sizeof(buf))) > 0) {
-                if (nwritten + nread > letters)
-                        nread = letters - nwritten;
-                if (write(STDOUT_FILENO, buf, nread) != nread) {
-                        close(fd);
-                        return (0);
-                }
-                nwritten += nread;
-                if (nwritten == letters)
-                        break;
-        }
+	buf = malloc(sizeof(char) * (letters + 1));
+	if (buf == NULL)
+		return (0);
 
-        close(fd);
+	read_count = read(fd, buf, letters);
+	if (read_count == -1)
+	{
+		free(buf);
+		close(fd);
+		return (0);
+	}
 
-        return (nwritten);
+	buf[read_count] = '\0';
+
+	write_count = write(STDOUT_FILENO, buf, read_count);
+	if (write_count == -1 || read_count != write_count)
+	{
+		free(buf);
+		close(fd);
+		return (0);
+	}
+
+	free(buf);
+	close(fd);
+
+	return (write_count);
 }
 
