ef26749326a163aa2602f268f4e31b5d2f9c64d1
issam.zouaid <issam.zouid14@gmail.com>
2023-04-14 15:08:04 -0700

    task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -19,12 +19,14 @@ int create_file(const char *filename, char *text_content)
 
         if (text_content != NULL)
         {
-                while (text_content[len] != '\0')
+                for (len = 0; text_content[len];)
+
                         len++;
         }
 
-        fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
         w = write(fd, text_content, len);
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+        
 
         if (fd == -1 || w == -1)
                 return (-1);
